f615a62eafa0a27223d6c90c6a6604f8d2f247f4
viktorplus <36795799+viktorplus@users.noreply.github.com>
2023-12-08 15:07:15 +0100

    Завдання 2: Додайте нову функціональність до проєкту «Чемпіонат Іспанії з футболу»:  Покажіть різницю забитих та пропущених голів для кожної команди.  Покажіть повну інформацію про матч.  Покажіть інформацію про матчі у конкретну дату.  Покажіть усі матчі конкретної команди.  Покажіть усіх гравців, які забили голи у конкретну дату.

--- Linq.cs
+++ Linq.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Threading.Tasks.Dataflow;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 
@@ -11,18 +11,9 @@ namespace SpanishFootballChampion
 {
     public static class Linq
     {
-        // 1. Get all players from the team with id 
-        public static void GetPlayersFromTeamWithId1(Champion context, int id)
+        public static void ShowGoalDifference(Champion context) //1
         {
-                var players = context.Players.Where(p => p.TeamId == id).ToList();
-                foreach (var player in players)
-                {
-                    player.Print();
-                }
-        }
 
-        public static void ShowGoalDifference(Champion context)
-        {
                 var teams = context.Teams.ToList();
                 var matches = context.Matches.ToList();
 
@@ -38,37 +29,126 @@ namespace SpanishFootballChampion
                                     .Where(match => match.LocalTeamId == team.Id || match.VisitorTeamId == team.Id)
                                     .Sum(match => match.LocalTeamId == team.Id ? match.VisitorGoals : match.LocalGoals),
                             };
+            Console.WriteLine("Разница голов всех команд:");
+            Console.WriteLine(new string('-', 100));
 
-                foreach (var item in query)
+            foreach (var item in query)
                 {
                     Console.Write($"Команда: {item.TeamName}");
                     Console.WriteLine($"\tРазница голов: {item.GoalsScored - item.GoalsConceded}");
-                    Console.WriteLine();
                 }
+            Console.WriteLine();
         }
 
-        public static void PrintMatches(Champion context)
+        public static void PrintMatches(Champion context) //2
         {
-                var matches = context.Matches.ToList();
+            var matches = context.Matches.ToList();
 
-                var query = from match in matches
-                            select new
-                            {
-                                Team1 = context.Teams.FirstOrDefault(team => team.Id == match.LocalTeamId)?.TeamName,
-                                Team2 = context.Teams.FirstOrDefault(team => team.Id == match.VisitorTeamId)?.TeamName,
-                                MatchDate = match.Date,
-                                LocalGoals = match.LocalGoals,
-                                VisitorGoals = match.VisitorGoals,
-                            };
+            var query = from match in matches
+                        select new
+                        {
+                            Team1 = context.Teams.FirstOrDefault(team => team.Id == match.LocalTeamId)?.TeamName,
+                            Team2 = context.Teams.FirstOrDefault(team => team.Id == match.VisitorTeamId)?.TeamName,
+                            MatchDate = match.Date,
+                            LocalGoals = match.LocalGoals,
+                            VisitorGoals = match.VisitorGoals,
+                        };
 
-                foreach (var item in query)
-                {
-                    Console.WriteLine($"{item.Team1}\t\t" +
-                                      $"{item.Team2}\t\t" +
-                                      $"{item.MatchDate}\t\t" +
-                                      $"{item.LocalGoals}\t\t\t" +
-                                      $"{item.VisitorGoals}\t\t\t");
-                }
+            Console.WriteLine($"{"Team1",-20}{"Team2",-20}{"MatchDate",-20}{"LocalGoals",-20}{"VisitorGoals",-20}");
+            Console.WriteLine(new string('-', 100));
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.Team1,-20}" +
+                                  $"{item.Team2,-20}" +
+                                  $"{item.MatchDate,-20}" +
+                                  $"{item.LocalGoals,-20}" +
+                                  $"{item.VisitorGoals,-20}");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintMatchesOfDay(Champion context, DateTime date) //3
+        {
+            var matchesOfDay = context.Matches
+                .Where(match => match.Date.Date == date.Date)
+                .ToList();
+
+            Console.WriteLine($"Matches on {date.ToShortDateString()}");
+            Console.WriteLine($"{"Team1",-20}{"Team2",-20}{"MatchDate",-20}{"LocalGoals",-20}{"VisitorGoals",-20}");
+            Console.WriteLine(new string('-', 100));
+
+            foreach (var match in matchesOfDay)
+            {
+                var team1 = context.Teams.FirstOrDefault(team => team.Id == match.LocalTeamId)?.TeamName;
+                var team2 = context.Teams.FirstOrDefault(team => team.Id == match.VisitorTeamId)?.TeamName;
+
+                Console.WriteLine($"{team1,-20}" +
+                                  $"{team2,-20}" +
+                                  $"{match.Date,-20}" +
+                                  $"{match.LocalGoals,-20}" +
+                                  $"{match.VisitorGoals,-20}");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void PrintMatchesForTeam(Champion context, int teamId) //4
+        {
+            var matchesForTeam = context.Matches
+                .Where(match => match.LocalTeamId == teamId || match.VisitorTeamId == teamId)
+                .ToList();
+
+            Console.WriteLine($"Matches for Team with Id {teamId}");
+            Console.WriteLine($"{"MatchId",-10}{"LocalTeam",-20}{"VisitorTeam",-20}{"MatchDate",-20}{"LocalGoals",-20}{"VisitorGoals",-20}");
+            Console.WriteLine(new string('-', 100));
+
+            foreach (var match in matchesForTeam)
+            {
+                var localTeam = context.Teams.FirstOrDefault(team => team.Id == match.LocalTeamId)?.TeamName;
+                var visitorTeam = context.Teams.FirstOrDefault(team => team.Id == match.VisitorTeamId)?.TeamName;
+
+                Console.WriteLine($"{match.Id,-10}" +
+                                  $"{localTeam,-20}" +
+                                  $"{visitorTeam,-20}" +
+                                  $"{match.Date,-20}" +
+                                  $"{match.LocalGoals,-20}" +
+                                  $"{match.VisitorGoals,-20}");
+            }
+
+            Console.WriteLine();
+        }
+
+
+        public static void PrintScoringPlayersByDate(Champion context, DateTime date) //5
+        {
+            var matches = context.Matches
+                .Where(match => match.Date.Date == date.Date)
+                .ToList();
+
+            var players = context.Players.ToList();
+
+            var query = from match in matches
+                        join player in players on match.LocalTeamId equals player.TeamId
+                        where match.LocalGoals > 0
+                        select new
+                        {
+                            PlayerName = player.Name,
+                            TeamName = context.Teams.FirstOrDefault(team => team.Id == match.LocalTeamId)?.TeamName,
+                            Goals = match.LocalGoals,
+                        };
+
+            Console.WriteLine($"{"PlayerName",-20}{"TeamName",-20}{"Goals",-20}");
+            Console.WriteLine(new string('-', 100));
+
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.PlayerName,-20}" +
+                                  $"{item.TeamName,-20}" +
+                                  $"{item.Goals,-20}");
+            }
+
+            Console.WriteLine();
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -4,50 +4,64 @@
     {
         static void Main(string[] args)
         {
+            DateTime dateToPrint = new DateTime(2023, 12, 07);
             using (var db = new Champion())
             {
-                string choice = Console.ReadLine(); 
-                Console.WriteLine("Выберите действие:");
-                Console.WriteLine("1. Показать разницу голов команд");
-                Console.WriteLine("2. Показать все матчи");
-                Console.WriteLine("3. Показать матчи за определенную дату");
-                Console.WriteLine("4. Показать всех игроков");
-
-                switch (choice)
+                while (true)
                 {
-                    case "1":
-                        Linq.ShowGoalDifference(db);
-                        break;
-                    case "2":
-                        Linq.PrintMatches(db);
+                    Console.WriteLine("Выберите действие:");
+                    Console.WriteLine("1. Показать разницу голов команд");
+                    Console.WriteLine("2. Показать все матчи");
+                    Console.WriteLine("3. Показать матчи за определенную дату");
+                    Console.WriteLine("4. Показать матчи команды");
+                    Console.WriteLine("5. Показать игроков, забивших голы за определенную дату");
 
-                        break;
-                    case "3":
-                        //ShowMatchOfData(db);
-                        break;
-                    case "4":
-                        //PrintAll(db);
-                        break;
-                    case "5":
-                        //PrintStudios(db);
-                        break;
-                    case "6":
-                        Linq.GetPlayersFromTeamWithId1(db, 1);
-                        break;
-                    case "7":
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Неверный выбор. Пожалуйста, выберите от 1 до 7.");
-                        break;
+                    string choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                            Linq.ShowGoalDifference(db);
+                            break;
+                        case "2":
+                            Linq.PrintMatches(db);
+
+                            break;
+                        case "3":
+                            Linq.PrintMatchesOfDay(db, dateToPrint);
+                            break;
+                        case "4":
+                            Linq.PrintMatchesForTeam(db,1);
+                            break;
+                        case "5":
+                            Linq.PrintScoringPlayersByDate(db, dateToPrint);
+                            break;
+                        case "6":
+
+                            break;
+                        case "7":
+                            
+                            break;
+                        case "8":
+
+                            break;
+                        case "9":
+
+                            break;
+                        case "10":
+
+                            break;
+                        case "11":
+
+                            break;
+                        case "12":
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Неверный выбор. Пожалуйста, выберите от 1 до 7.");
+                            break;
+                    }
+                    
                 }
-
-
-
-
-
-
-
             }
         }
     }
